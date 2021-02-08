5bcc53e0d9f7ec966f158b704fb3d892b2bdf206
Rafael Dona <rafa_dona@hotmail.com>
2021-02-08 14:57:56 -0300

    contentful running

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import Layout from "../componnents/Layout"
 import styles from "../css/products.module.css"
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
 
-const products = () => {
+
+const products = ({ data }) => {
+    const { allContentfulProduct: { nodes: products } } = data;
     return (
         <Layout>
             <div className={styles.page}>
 
-                <h1>products page</h1>
-                <p className={styles.text}>I shall be late!' (when she thought it over afterwards, it occurred to her to wink with one finger pressed upon its forehead (the position in which you usually see Shakespeare, in the pictures of him), while the rest waited in silence. She took down a large rabbit-hole under the door; so either way I'll get into the garden with one eye; but to get through was more hopeless than ever: she sat down at once, in a great hurry to change the subject of conversation. The question is, what did the archbishop find?' The Mouse gave a sudden leap out of the way out of the water, and seemed to quiver all over with fright. `But it's no use now,' thought poor Alice, `to pretend to be a person of authority among them, called out, `Sit down, all of you, and listen to her, still it was empty: she did not come the same as they used to do:-- `How doth the little golden key in the sea, some children digging in the pool as it went. Why, I wouldn't say anything about it, even if I like being that person, I'll come up: if not, I'll stay down here till I'm somebody else"--but, oh dear!' cried Alice in a soothing tone: `don't be angry about it.
-</p>
+                {products.map((product) => {
+                    return <article key={product.id}>
+                        <Image fluid={product.image.fluid} alt={product.title} />
+                        <h3>{product.title} <span>R${product.price}</span></h3>
+                        <Link to={`/products/${product.slug}`}>more details</Link>
+                    </article>
+
+                })}
+
             </div>
 
         </Layout>
@@ -17,4 +28,23 @@ const products = () => {
     )
 }
 
-export default products;
+export const query = graphql`
+  {
+    allContentfulProduct {
+      nodes {
+        id
+        price
+        title
+        image {
+          fluid(maxWidth:5000) {
+            ...GatsbyContentfulFluid
+          }
+        }
+        slug
+      }
+    }
+  }
+`
+
+export default products
+
