627cefa71071b6d8d3c288823bc7277426c77c78
Rafael Dona <rafa_dona@hotmail.com>
2021-02-08 15:30:24 -0300

    gatsby node

---
+++ src/templates/productTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const productTemplate = (props) => {
+    return (
+        <div>
+            <h1>Hello from product template</h1>
+        </div>
+    )
+}
+
+export default productTemplate
