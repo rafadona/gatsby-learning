687317db647c86499f25d86b6f7efdf76faf1f71
Rafael Dona <rafa_dona@hotmail.com>
2021-02-15 16:47:23 -0300

    r

---
+++ src/componnents/NavBar.js
@@ -0,0 +1,29 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home </Link>
+                </li>
+                <li>
+                    <Link to="/blog/">Blog</Link>
+                </li>
+                <li>
+                    <Link to="/products/">Products</Link>
+                </li>
+                <li>
+                    <Link to="/examples">Examples</Link>
+                </li>
+                <li>
+                    <Link to="/images">images</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
