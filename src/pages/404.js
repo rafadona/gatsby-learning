51a82ad7dc2bceea7ed5cec6e96b7acb4b6f7a07
Rafael Dona <rafa_dona@hotmail.com>
2021-02-16 18:42:59 -0300

    test

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../componnents/Layout"
 export default function error() {
     return (
         <Layout>
-            <h1>error page</h1>
+            <h1>error test page</h1>
         </Layout>
     )
 }
